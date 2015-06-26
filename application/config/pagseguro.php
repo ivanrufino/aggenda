410e26590d11cfced61041f4def5536441cc996c
Ivan Rufino Martins <ivan.rufino.m@gmail.com>
2015-06-26 18:00:34 -0300

    escritorio

---
+++ application/config/pagseguro.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['pagseguroAccount'] = 'ivan.rufino.m@gmail.com'; // email de acesso ao painel do pagseguro.
+$config['pagseguroToken'] = '6878FED47B9242C588F19484794F585C'; // token do pagseguro
---
+++ application/logs/log-2015-06-26.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-06-26 17:38:30 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:38:30 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:38:55 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:38:55 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:39:06 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:39:06 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:40:29 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:40:29 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:41:42 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:41:42 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 34
+ERROR - 2015-06-26 17:43:30 --> Unable to load the requested class: Teste
+ERROR - 2015-06-26 17:44:46 --> Severity: Notice --> Undefined property: Pagseguro::$teste /var/www/aggenda/application/controllers/Pagseguro.php 16
+ERROR - 2015-06-26 17:44:46 --> Severity: Error --> Call to a member function testar() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 16
+ERROR - 2015-06-26 17:44:51 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 35
+ERROR - 2015-06-26 17:44:51 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 35
+ERROR - 2015-06-26 17:45:28 --> Non-existent class: Pagseguro2
+ERROR - 2015-06-26 17:45:47 --> Non-existent class: Pagseguro2
+ERROR - 2015-06-26 17:46:07 --> Non-existent class: Pagseguro2
+ERROR - 2015-06-26 17:46:33 --> Non-existent class: Pagseguros
+ERROR - 2015-06-26 17:46:54 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 35
+ERROR - 2015-06-26 17:46:54 --> Severity: Error --> Call to a member function set_user() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 35
+ERROR - 2015-06-26 17:47:07 --> Severity: Notice --> Undefined property: Pagseguro::$pagseguro /var/www/aggenda/application/controllers/Pagseguro.php 53
+ERROR - 2015-06-26 17:47:07 --> Severity: Error --> Call to a member function set_products() on a non-object /var/www/aggenda/application/controllers/Pagseguro.php 53
+ERROR - 2015-06-26 17:56:01 --> Unable to load the requested class: Pagseguros
+ERROR - 2015-06-26 17:56:05 --> 404 Página não encontrada: Exemplo/index
+ERROR - 2015-06-26 17:56:16 --> 404 Página não encontrada: Ofertas/index
+ERROR - 2015-06-26 17:56:31 --> 404 Página não encontrada: Ofertas/index
+ERROR - 2015-06-26 17:56:45 --> 404 Página não encontrada: Ofertas/index
