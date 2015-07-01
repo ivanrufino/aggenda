f453f75de3144bce7ec20388a4a2ffc774bc863c
Ivan Rufino Martins <ivan.rufino.m@gmail.com>
2015-07-01 17:14:44 -0300

    escritorio

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,12 +9,14 @@ class Admin extends CI_Controller {
     private $cod_associado;
     private $css = null;
     private $js = null;
+    private $tela = null; //telas do template
+    private $data = null; //dados com variaveis para a tela
 
     function __construct() {
         parent::__construct();
         $this->css = array(PASTA . '/layout2', '../' . PLUGIN . '/flot/examples/examples');
         /* Global js */
-        $js_global = array('../' . PLUGIN . '/bootstrap/js/bootstrap.min', '../' . PLUGIN . '/modernizr-2.6.2-respond-1.1.0.min','jquery.form');
+        $js_global = array('../' . PLUGIN . '/bootstrap/js/bootstrap.min', '../' . PLUGIN . '/modernizr-2.6.2-respond-1.1.0.min', 'jquery.form');
         $this->js = array_merge($js_global); //, array(PASTA.'/for_index'));
         if (!$this->session->has_userdata('logged') || $this->session->userdata('logged') != 'in') {
             //redirect('login');
@@ -22,41 +24,33 @@ class Admin extends CI_Controller {
         $this->cod_associado = $this->session->userdata('CODIGO');
         $this->load->model('Admin_model', 'admin');
         $this->load->model('Agenda_model', 'agenda');
+        if (is_null($this->data['associado'])) {
+            $this->data['associado'] = $this->admin->getAdmin($this->cod_associado);
+        }
     }
 
     public function index() {
-        $data['num_servico']=$this->agenda->getNumServicos($this->cod_associado);
-        $data['associado'] = $this->admin->getAdmin($this->cod_associado);
-        $data['titulo'] = 'Aggenda.com | admin';
-       
-        $tela['agendamento'] = ('modal/agendamento');
-        $tela['conteudo'] = ('associado/calendario');
-        $tela['menu'] = ('associado/menu');
-        $tela['lateralDireita'] = ('associado/vazio');
-        
-        if(is_null($data['associado']['COD_EMPRESA'])){          
-            $tela['conteudo'] = ('associado/config_inicial');
-            $tela['menu'] = ('associado/menu_vazio');
-            $tela['lateralDireita'] = ('associado/vazio');
-        }else{ 
-            $data['servicos'] = $this->agenda->getServicos($this->cod_associado);
-            $data['horario'] = $this->agenda->getHorario($this->cod_associado);
-            }
-        //$dados['COD_EMPRESA']=$data['associado']['COD_EMPRESA'];
-        //$this->session->set_userdata($dados);
+        $this->dadosComuns();
+        $this->data['titulo'] = 'Aggenda.com | admin > Calendário';
+        $this->tela['agendamento'] = ('modal/agendamento');
+        $this->tela['lateralDireita'] = ('associado/vazio');
+        if (is_null($this->data['associado']['COD_EMPRESA'])) {
+            $this->tela['conteudo'] = ('associado/config_inicial');
+            $this->tela['menu'] = ('associado/menu_vazio');
+        } else {
+            $this->tela['conteudo'] = ('associado/calendario');
+            $this->data['servicos'] = $this->agenda->getServicos($this->cod_associado);
+            $this->data['horario'] = $this->agenda->getHorario($this->cod_associado);
+        }
 
-
-        $this->parser->adc_css($this->css);
-        $this->parser->adc_js($this->js);
-        $this->parser->mostrar('template/template_associado.php', $tela, $data);
+        $this->parser->mostrar('template/template_associado.php', $this->tela, $this->data);
     }
-    
-    
+
     public function importacao() {
         $this->load->helper('file');
         $string = read_file('./assets/importacao/usuario.csv');
         $row = 1;
-        $array=array();
+        $array = array();
         $handle = fopen('./assets/importacao/usuario.csv', "r");
         while (($data = fgetcsv($handle, 2000, ",")) !== FALSE) {
             $num = count($data);
@@ -65,7 +59,7 @@ class Admin extends CI_Controller {
             for ($c = 0; $c < $num; $c++) {
                 echo $data[$c] . "<br />\n";
             }
-            $array[]= $data ;
+            $array[] = $data;
         }
         fclose($handle);
         print_r($array);
@@ -73,37 +67,33 @@ class Admin extends CI_Controller {
     }
 
     public function painel() {
-         $data['associado'] = $this->admin->getAdmin($this->cod_associado);
-        $data['titulo'] = 'Aggenda.com | admin' ;
-        $tela['conteudo'] = 'vazio';
-        $tela['menu'] = 'associado/menu';
-        $tela['lateralDireita'] = 'associado/lateralDireita';
-        
-        
-        //$dados['COD_EMPRESA']=$data['associado']['COD_EMPRESA'];
-        //$this->session->set_userdata($dados);
-        
+        $this->dadosComuns();
+        $this->data['titulo'] = 'Aggenda.com | admin > Painel';
+        $this->tela['conteudo'] = 'vazio';
+        $this->tela['menu'] = 'associado/menu';
+        $this->tela['lateralDireita'] = 'associado/lateralDireita';        
 
-        $this->parser->adc_css($this->css);
-        $this->parser->adc_js($this->js);
-        $this->parser->mostrar('template/template_associado.php', $tela, $data);
+        $this->parser->mostrar('template/template_associado.php', $this->tela, $this->data);
     }
+
     public function Clientes() {
-         $data['associado'] = $this->admin->getAdmin($this->cod_associado);
-        $data['titulo'] = 'Aggenda.com | admin' ;
-        $tela['conteudo'] = 'vazio';
-        $tela['menu'] = 'associado/menu';
-        $tela['lateralDireita'] = 'associado/lateralDireita';
-        
-        
-        //$dados['COD_EMPRESA']=$data['associado']['COD_EMPRESA'];
-        //$this->session->set_userdata($dados);
-        
+        $this->dadosComuns();
 
+        $this->data['titulo'] = 'Aggenda.com | admin > Clientes';
+        $this->tela['conteudo'] = 'vazio';
+        $this->tela['lateralDireita'] = 'associado/lateralDireita';       
+
+        $this->parser->mostrar('template/template_associado.php', $this->tela, $this->data);
+    }
+
+    public function dadosComuns() {
         $this->parser->adc_css($this->css);
         $this->parser->adc_js($this->js);
-        $this->parser->mostrar('template/template_associado.php', $tela, $data);
+        $this->data['num_servico'] = $this->agenda->getNumServicos($this->cod_associado);
+        $this->tela['menu'] = 'associado/menu';
+        
     }
+
 }
 
 // Admin.php
